f7ffa1e8da1b631e5b8b33d2e6eab8223eed2241
stepan <fiturehappyman@proton.me>
2025-06-30 19:51:59 +0200

    Update JavaFileExecutor.java
    corrected comments.

--- process_simulator/src/JavaFileExecutor.java
+++ process_simulator/src/JavaFileExecutor.java
@@ -14,7 +14,7 @@ public class JavaFileExecutor {
 
    public JavaFileExecutor(int totalMemory, int quantum) {            // simulator that gives us the quantum of time for each process on cpu, and totalMemoy on the system.
       this.memoryManager = new MemoryManager(totalMemory);            // memoryManager initialised and intialises memoryBlocks
-      this.scheduler = new ProcessScheduler(quantum, memoryManager);  // scheduler initialised and initialises quese for ready processes, list for NEW process, memory BLocks
+      this.scheduler = new ProcessScheduler(quantum, memoryManager);  // scheduler initialised and initialises queues for ready processes, list for NEW process, memory Blocks
    }
 
    /* adds processes as NEW to processes ArrayList */
